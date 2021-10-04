ffc0c1a0a67e04b6e26b1785005ea04e2e0d718c
Gabrielius Bertašius <gabrielius@Gabrielius-MacBook-Air.lan>
2021-10-04 12:33:02 +0300

    Docker amendments

--- backend/db/01-init.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-export PGPASSWORD=$POSTGRES_PASSWORD;
-chmod +x db/01-init.sh
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-  CREATE DATABASE IMAGES;
-  COMMIT;
-EOSQL
